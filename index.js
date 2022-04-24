26eb947e138f6c6137eba36eecb42c9c88b2e527
Samuel Gonzalez <samglz10@gmail.com>
2022-04-23 20:23:00 -0400

    eventlistener being weird

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ document.body.append(newImage);
 
 return newImage
 }
-
-let url='assets/boulder.png'
-let left='400'
-let bottom='100'
-newImage(url, left, bottom);
+let greenCharacter = document.createElement('img')
+    greenCharacter.src = 'assets/green-character.gif'
+    greenCharacter.style.position = 'fixed'
+    greenCharacter.style.left = '100px'
+    greenCharacter.style.bottom = '100px'
+    document.body.append(greenCharacter)
 
 //assets/tree.png (200px from the left, 300px from the bottom)
 newImage('assets/tree.png', '200', '300');
@@ -25,6 +26,35 @@ newImage('assets/crate.png', '150', '200');
 //assets/well.png (500px from the left, 425px from the bottom)
 newImage('assets/well.png', '500', '425');
 
+
+const newItem=(url, left, bottom) => {
+
+    let newItem = document.createElement('img')
+newItem.src = url
+newItem.style.position = 'fixed'
+newItem.style.left = left + 'px'
+newItem.style.bottom = bottom + 'px'
+document.body.append(newItem);
+
+
+return newItem;
+}
+
+
+
+
+// assets/sword.png placed 500px from the left and 405px from the bottom.
+newItem('assets/sword.png', '500', '405')
+//assets/shield.png (165px from the left, 185px from the bottom)
+//azat there is a typo for the shield.png
+newItem ('assets/sheild.png', '165','185')
+//assets/staff.png (600px from the left, 100px from the bottom)
+newItem('assets/staff.png', '600', '100')
+
+newItem.addEventListener('click', function() {
+    newItem.remove()})
+
+/*
 let sword = document.createElement('img')
 sword.src = 'assets/sword.png'
 sword.position = 'fixed'
@@ -34,25 +64,13 @@ document.body.append(sword)
 
 sword.addEventListener('click', function(){
     sword.remove()
-})
+
+*/
 
 
 
 
 
 
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
 
-
-/*let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree) */
 
